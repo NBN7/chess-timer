b244ea0a410cf47c25e463095b1bc4cf9323e8cf
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-09-06 20:19:03 -0300

    update select style

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -6,7 +6,7 @@ interface Props extends HTMLAttributes<HTMLSelectElement> {
 
 export const Select = ({ children, ...props }: Props) => {
   const selectClassName =
-    "appearance-none w-full h-[55px] p-4 rounded-2xl overflow-scroll cursor-pointer";
+    "appearance-none w-full h-[55px] bg-[#3B3B3B] p-4 rounded-2xl overflow-scroll cursor-pointer";
 
   return (
     <select {...props} className={selectClassName}>
