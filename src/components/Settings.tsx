0a6026fa7992721da9e2b9907b99c63316de4092
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-09-06 20:23:05 -0300

    update settings responsive style

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -57,8 +57,8 @@ const Settings = memo(
     };
 
     return (
-      <div className="flex items-center justify-center absolute w-full h-full backdrop-blur-sm">
-        <div className="flex flex-col items-center w-[300px] h-[300px] bg-[#242424] rounded-2xl p-4 animate-fade-up">
+      <div className="flex items-center justify-center absolute w-full h-full backdrop-blur-sm p-2">
+        <div className="flex flex-col items-center w-[300px] h-[300px] bg-[#242424] rounded-2xl p-4">
           <ExitBtn />
           <div className="flex flex-col w-full h-full justify-center gap-4">
             <TimeOptions />
